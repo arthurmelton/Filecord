2057d08df69341f6d54ea47e2333a7e2262a6e4c
arthurmelton <amtitan@icloud.com>
2022-06-28 14:21:57 -0400

    smaller urls and shows content length

--- server/src/main.rs
+++ server/src/main.rs
@@ -35,17 +35,25 @@ fn main() {
                 .collect::<String>()
                 .trim()
                 .to_string();
-            let mut writer = Vec::new();
-            let mut z = ZlibDecoder::new(writer);
-            z.write_all(&base64::decode(&url).unwrap()).unwrap();
-            writer = z.finish().unwrap();
-            let text = String::from_utf8(writer.clone()).expect("String parsing error");
-            let decompressed: Vec<&str> = text.split("&").collect();
+            let char_list: Vec<char> = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".chars().collect();
+            let first_length = char_list.iter().position(|c| *c == url.chars().nth(0).unwrap()).unwrap();
+            let mut index = 1;
+            let mut returns = [0 as u64; 2];
+            let mut real_first_length = 0;
+            for x in 0..2 {
+                let mut num:u64 = 0;
+                while (x == 0 && num.to_string().len() < first_length) || (x == 1 && url.len() > index) {
+                    num+=char_list.iter().position(|c| *c == url.chars().nth(index).unwrap()).unwrap() as u64*(62_u64.pow(index as u32-1-(x*real_first_length - x*1) as u32));
+                    index+=1;
+                }
+                returns[x as usize] = num;
+                real_first_length = index;
+            }
             let mut buffer = Vec::new();
             ureq::get(&format!(
                 "https://cdn.discordapp.com/attachments/{}/{}/data",
-                decompressed.get(0).unwrap(),
-                decompressed.get(1).unwrap()
+                returns[0],
+                returns[1]
             ))
                 .call()
                 .unwrap()
@@ -62,7 +70,20 @@ fn main() {
             decompressed.remove(0);
             let channel = decompressed[0];
             decompressed.remove(0);
-            stream.write(format!("HTTP/1.1 200 Ok\r\nContent-Disposition: attachment; filename=\"{}\"\r\n\r\n", file_name).as_bytes()).unwrap();
+            let mut length:usize = 8388608*(decompressed.len()-1);
+            let mut buffer = Vec::new();
+            ureq::get(&format!(
+                "https://cdn.discordapp.com/attachments/{}/{}/part_{}",
+                channel, decompressed.last().unwrap(), decompressed.len()-1
+            ))
+                .call()
+                .unwrap()
+                .into_reader()
+                .read_to_end(&mut buffer)
+                .unwrap();
+            length += buffer.len();
+            drop(buffer);
+            stream.write(format!("HTTP/1.1 200 Ok\r\nContent-Disposition: attachment; filename=\"{}\"\r\nContent-Length: {}\r\n\r\n", file_name, length).as_bytes()).unwrap();
             let mut index = 0;
             for id in decompressed {
                 let mut buffer = Vec::new();
