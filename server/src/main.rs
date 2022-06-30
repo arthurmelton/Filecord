e3f93d233bc35c1e9130228851f80d701e820fcb
Arthur Melton <amtitan@icloud.com>
2022-06-30 09:15:07 -0400

    use a discord webhook to upload files

--- server/src/main.rs
+++ server/src/main.rs
@@ -53,15 +53,27 @@ fn main() {
                 .collect::<String>()
                 .trim()
                 .to_string();
-            let char_list: Vec<char> = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".chars().collect();
-            let first_length = char_list.iter().position(|c| *c == url.chars().nth(0).unwrap()).unwrap();
+            let char_list: Vec<char> =
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+                    .chars()
+                    .collect();
+            let first_length = char_list
+                .iter()
+                .position(|c| *c == url.chars().nth(0).unwrap())
+                .unwrap();
             let mut index = 1;
             let mut returns = [0 as u64; 2];
             let mut real_first_length = 0;
             for x in 0..2 {
                 let mut num: u64 = 0;
-                while (x == 0 && num.to_string().len() < first_length) || (x == 1 && url.len() > index) {
-                    num += char_list.iter().position(|c| *c == url.chars().nth(index).unwrap()).unwrap() as u64 * (62_u64.pow(index as u32 - 1 - (x * real_first_length - x * 1) as u32));
+                while (x == 0 && num.to_string().len() < first_length)
+                    || (x == 1 && url.len() > index)
+                {
+                    num += char_list
+                        .iter()
+                        .position(|c| *c == url.chars().nth(index).unwrap())
+                        .unwrap() as u64
+                        * (62_u64.pow(index as u32 - 1 - (x * real_first_length - x * 1) as u32));
                     index += 1;
                 }
                 returns[x as usize] = num;
@@ -70,8 +82,7 @@ fn main() {
             let mut buffer = Vec::new();
             ureq::get(&format!(
                 "https://cdn.discordapp.com/attachments/{}/{}/data",
-                returns[0],
-                returns[1]
+                returns[0], returns[1]
             ))
                 .call()
                 .unwrap()
@@ -86,7 +97,7 @@ fn main() {
             let mut decompressed: Vec<&str> = text.split("&").collect();
             let file_name = decode(decompressed[0]).unwrap();
             decompressed.remove(0);
-            let channel = decompressed[0];
+            let length = decompressed[0];
             decompressed.remove(0);
             if messaging_platform {
                 stream.write(format!("HTTP/1.1 200 Ok\r\nContent-Length: 240\r\nContent-Type: text/html; charset=UTF-8\r\n\r
@@ -99,25 +110,13 @@ fn main() {
 <body>
 </body>", file_name).as_bytes()).unwrap();
             } else {
-                let mut length: usize = 8388608 * (decompressed.len() - 1);
-                let mut buffer = Vec::new();
-                ureq::get(&format!(
-                    "https://cdn.discordapp.com/attachments/{}/{}/part_{}",
-                    channel, decompressed.last().unwrap(), decompressed.len() - 1
-                ))
-                    .call()
-                    .unwrap()
-                    .into_reader()
-                    .read_to_end(&mut buffer)
-                    .unwrap();
-                length += buffer.len();
                 stream.write(format!("HTTP/1.1 200 Ok\r\nContent-Disposition: attachment; filename=\"{}\"\r\nContent-Length: {}\r\n\r\n", file_name, length).as_bytes()).unwrap();
                 let mut index = 0;
                 for id in decompressed {
                     let mut buffer = Vec::new();
                     ureq::get(&format!(
                         "https://cdn.discordapp.com/attachments/{}/{}/part_{}",
-                        channel, id, index
+                        returns[0], id, index
                     ))
                         .call()
                         .unwrap()
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,28 @@
+use std::{process, thread};
 use std::fs::File;
 use std::io::{Read, Write};
 use std::io::{Seek, SeekFrom};
 use std::path::PathBuf;
-use std::process;
+use std::time::Duration;
 
 use clap::{App, Arg};
 use flate2::Compression;
 use flate2::write::ZlibEncoder;
-use lazy_static::lazy_static;
 use pbr::ProgressBar;
-use serenity::async_trait;
-use serenity::Client;
-use serenity::client::EventHandler;
-use serenity::model::id::ChannelId;
-use serenity::model::prelude::Ready;
-use serenity::prelude::Context;
+use rand::prelude::IteratorRandom;
+use serde_json::Value;
 
-lazy_static! {
-    pub static ref MATCHES: clap::parser::ArgMatches = App::new("Sharex")
+fn main() {
+    let matches: clap::parser::ArgMatches = App::new("Sharex")
         .version("1.0")
         .about("Share big files on discord")
         .arg(
-            Arg::with_name("Token")
-                .short('t')
-                .long("token")
-                .help("The discord bot token")
+            Arg::with_name("Webhook")
+                .short('w')
+                .long("webhook")
+                .help("the url to the webhook")
                 .takes_value(true)
-                .required(true)
-        )
-        .arg(
-            Arg::with_name("Application Id")
-                .short('a')
-                .long("application")
-                .help("The discord bot application id")
-                .takes_value(true)
-                .required(true)
-        )
-        .arg(
-            Arg::with_name("Channel")
-                .short('c')
-                .long("channel")
-                .help("The channel id that the files will be uploaded to")
-                .takes_value(true)
-                .required(true)
+                .required(true),
         )
         .arg(
             Arg::with_name("Input")
@@ -50,125 +30,148 @@ lazy_static! {
                 .long("input")
                 .help("The file to upload")
                 .takes_value(true)
-                .required(true)
+                .required(true),
         )
         .get_matches();
-}
-
-#[tokio::main]
-async fn main() {
-    let mut client = Client::builder(MATCHES.value_of("Token").unwrap())
-        .event_handler(Handler)
-        .application_id(MATCHES.value_of("Application Id").unwrap().parse().unwrap())
-        .await
-        .expect("Error creating client");
-
-    if let Err(why) = client.start().await {
-        println!("Client error: {:?}", why);
-    }
-}
-
-pub struct Handler;
-
-#[async_trait]
-impl EventHandler for Handler {
-    async fn ready(&self, ctx: Context, _ready: Ready) {
-        let channel = ChannelId(
-            MATCHES
-                .value_of("Channel")
+    let mut file = File::open(matches.value_of("Input").unwrap()).expect("no file found");
+    if file.metadata().unwrap().len() < 8388608 {
+        println!("The file is less than 8mb you can just upload it your self");
+    } else {
+        let total = (file.metadata().unwrap().len() as f64 / 8388608.0).ceil() as usize;
+        let mut pb = ProgressBar::new((total * 2) as u64);
+        pb.format("╢▌▌░╟");
+        let mut i = 0;
+        let channel: Value = serde_json::from_str(
+            ureq::get(matches.value_of("Webhook").unwrap())
+                .call()
                 .unwrap()
-                .parse()
-                .expect("Channel needs to be a number"),
+                .into_string()
+                .unwrap()
+                .as_str(),
+        )
+            .unwrap();
+        let channel = channel["channel_id"]
+            .as_str()
+            .unwrap()
+            .parse::<u64>()
+            .unwrap();
+        let mut returns = format!(
+            "{}&{}",
+            urlencoding::encode(
+                PathBuf::from(matches.value_of("Input").unwrap())
+                    .file_name()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+            ),
+            file.metadata().unwrap().len()
         );
-        let mut file = File::open(MATCHES.value_of("Input").unwrap()).expect("no file found");
-        if file.metadata().unwrap().len() < 8388608 {
-            println!("The file is less than 8mb you can just upload it your self");
-        } else {
-            let total = (file.metadata().unwrap().len() as f64 / 8388608.0).ceil() as usize;
-            let mut pb = ProgressBar::new((total * 2) as u64);
-            pb.format("╢▌▌░╟");
-            let mut i = 0;
-            let mut returns = format!(
-                "{}&{}",
-                urlencoding::encode(
-                    PathBuf::from(MATCHES.value_of("Input").unwrap())
-                        .file_name()
-                        .unwrap()
-                        .to_str()
-                        .unwrap()
-                ),
-                MATCHES.value_of("Channel").unwrap()
-            );
-            while total - i > 0 {
-                let mut buf = Vec::new();
-                let mut temp = file.try_clone().expect("cant clone").take(8388608);
-                file.seek(SeekFrom::Start((8388608 * i) as u64))
-                    .expect("Failed to seek");
-                temp.read_to_end(&mut buf).expect("Didn't read enough");
-                pb.inc();
-                let mut message = None;
-                let mut attachments = None;
-                let mut go_again = true;
-                while go_again {
-                    message = Some(
-                        channel
-                            .send_message(&ctx.http, |m| {
-                                m.add_file((buf.as_slice(), format!("part_{}", i).as_str()));
-                                m
-                            })
-                            .await,
+        let mut gone = 0;
+        while file.metadata().unwrap().len() > gone {
+            let mut boundary = "--------".to_string();
+            let mut rng = rand::thread_rng();
+            for _ in 0..20 {
+                boundary.push("0123456789".chars().choose(&mut rng).unwrap());
+            }
+            let mut buf = Vec::new();
+            for i in format!("--{}\r\nContent-Disposition: form-data; name=\"file1\"; filename=\"{}\"\r\nContent-Type: application/octet-stream\r\n\r\n", boundary, format!("part_{}", i)).as_bytes() {
+                buf.push(*i);
+            }
+            let offset = buf.len() + 34;
+            let mut temp = file
+                .try_clone()
+                .expect("cant clone")
+                .take((8388608 - offset) as u64);
+            file.seek(SeekFrom::Start(gone)).expect("Failed to seek");
+            gone += (8388608 - offset) as u64;
+            temp.read_to_end(&mut buf).unwrap();
+            for i in format!("\r\n--{}--", boundary).as_bytes() {
+                buf.push(*i);
+            }
+            pb.inc();
+            let mut response: Option<Value> = None;
+            loop {
+                let x = ureq::post(matches.value_of("Webhook").unwrap())
+                    .set(
+                        "content-type",
+                        &format!("multipart/form-data; boundary={}", boundary),
+                    )
+                    .send_bytes(buf.as_slice());
+                if x.is_ok() {
+                    response = Some(
+                        serde_json::from_str(x.unwrap().into_string().unwrap().as_str()).unwrap(),
                     );
-                    go_again = message.as_ref().unwrap().is_err();
-                    if !go_again {
-                        attachments = Some(message.unwrap().unwrap().attachments);
-                    }
-                }
-                pb.inc();
-                for i in attachments.unwrap() {
-                    returns.push_str(format!("&{}", &i.id.0.to_string()).as_str());
-                }
-                i += 1;
+                    break;
+                } else {
+                    println!("{:?}", x);
+                    thread::sleep(Duration::from_secs(1));
+                };
             }
-            let base = "https://amtitan-sharex.herokuapp.com/";
-            let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
-            e.write_all(returns.as_bytes()).unwrap();
-            let data = e.finish().unwrap();
-            let mut message = None;
-            let mut attachments = None;
-            let mut go_again = true;
-            while go_again {
-                message = Some(
-                    channel
-                        .send_message(&ctx.http, |m| {
-                            m.add_file((data.as_slice(), "data"));
-                            m
-                        })
-                        .await,
-                );
-                go_again = message.as_ref().unwrap().is_err();
-                if !go_again {
-                    attachments = Some(message.unwrap().unwrap().attachments);
-                }
+            pb.inc();
+            for i in response.unwrap()["attachments"].as_array().unwrap() {
+                returns.push_str(format!("&{}", i["id"].as_str().unwrap()).as_str());
             }
-            let char_list: Vec<char> =
-                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-                    .chars()
-                    .collect();
-            let mut returns = "".to_string();
-            returns.push(*char_list.get(channel.0.to_string().len()).unwrap());
-            for num in [channel.0, attachments.unwrap()[0].id.0] {
-                let mut index = 0;
-                loop {
-                    let x = num / (62_u64.pow(index)) % 62;
-                    returns.push(*char_list.get(x as usize).unwrap());
-                    index += 1;
-                    if index == 11 {
-                        break;
-                    }
-                }
-            }
-            pb.finish_println(format!("{}{}\n", base, returns).as_str());
+            i += 1;
         }
-        process::exit(1);
+        let base = "https://amtitan-sharex.herokuapp.com/";
+        let mut boundary = "--------".to_string();
+        let mut rng = rand::thread_rng();
+        for _ in 0..20 {
+            boundary.push("0123456789".chars().choose(&mut rng).unwrap());
+        }
+        let mut buf = Vec::new();
+        for i in format!("--{}\r\nContent-Disposition: form-data; name=\"file1\"; filename=\"data\"\r\nContent-Type: application/octet-stream\r\n\r\n", boundary).as_bytes() {
+            buf.push(*i);
+        }
+        let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+        e.write_all(returns.as_bytes()).unwrap();
+        for i in e.finish().unwrap() {
+            buf.push(i);
+        }
+        for i in format!("\r\n--{}--", boundary).bytes() {
+            buf.push(i);
+        }
+        let mut response: Option<Value> = None;
+        loop {
+            let x = ureq::post(matches.value_of("Webhook").unwrap())
+                .set(
+                    "content-type",
+                    &format!("multipart/form-data; boundary={}", boundary),
+                )
+                .send_bytes(buf.as_slice());
+            if x.is_ok() {
+                response =
+                    Some(serde_json::from_str(x.unwrap().into_string().unwrap().as_str()).unwrap());
+                break;
+            } else {
+                println!("{:?}", x);
+                thread::sleep(Duration::from_secs(1));
+            };
+        }
+        let char_list: Vec<char> = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+            .chars()
+            .collect();
+        let mut returns = "".to_string();
+        returns.push(*char_list.get(channel.to_string().len()).unwrap());
+        for num in [
+            channel,
+            response.unwrap()["attachments"][0]["id"]
+                .as_str()
+                .unwrap()
+                .parse::<u64>()
+                .unwrap(),
+        ] {
+            let mut index = 0;
+            loop {
+                let x = num / (62_u64.pow(index)) % 62;
+                returns.push(*char_list.get(x as usize).unwrap());
+                index += 1;
+                if index == 11 {
+                    break;
+                }
+            }
+        }
+        pb.finish_println(format!("{}{}\n", base, returns).as_str());
     }
+    process::exit(1);
 }
