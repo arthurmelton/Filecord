7df6008e2eb2d72c9ce9195ce1303d61d99b37b0
Arthur Melton <amtitan@icloud.com>
2022-07-13 10:45:08 -0400

    favicon

--- src/main.rs
+++ src/main.rs
@@ -125,6 +125,11 @@ fn main() {
         <meta property=\"og:type\" content=\"website\" />
         <meta name=\"description\" content=\"Sharex is a program to share large files for free using discord\" />
         <meta content=\"#1E293B\" name=\"theme-color\" />
+        <link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/image/favicon/apple-touch-icon.png\">
+        <link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"/image/favicon/favicon-32x32.png\">
+        <link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"/image/favicon/favicon-16x16.png\">
+        <link rel=\"manifest\" href=\"/image/favicon/site.webmanifest\">
+        <meta property=\"og:image\" content=\"/image/favicon/favicon.png\" />
     </head>
     <body>", file_name);
                     stream.write(format!("HTTP/1.1 200 Ok\r\nContent-Length: {}\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n{}", send.len(), send).as_bytes()).unwrap();
